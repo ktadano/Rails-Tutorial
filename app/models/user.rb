6b8afb754f0c7bf70fe3329435869cdede52ee97
ktadano <ktadano@ktadano-iMac.local>
2016-05-23 17:48:50 +0900

    access @user for inside

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   class << self
     def digest(string)
       cost = ActiveModel::SecurePassword.min_cost ?
-        BCrypt::Engine::MIN_COST:
+        BCrypt::Engine::MIN_COST :
         BCrypt::Engine.cost
       BCrypt::Password.create(string, cost: cost)
     end
